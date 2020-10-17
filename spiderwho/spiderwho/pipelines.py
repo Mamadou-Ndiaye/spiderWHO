710bc06bb00bbbd0ad7230bcba1496ac47f70b15
Mamadou-Ndiaye <ndiamamadou@gmail.com>
2020-10-17 00:24:05 +0000

    creation de la base de donnees sqlite3 ,table ,insertion de donnnes  dans en utilisant le fichier pipeline

--- spiderwho/spiderwho/pipelines.py
+++ spiderwho/spiderwho/pipelines.py
@@ -5,9 +5,37 @@
 
 
 # useful for handling different item types with a single interface
+import sqlite3
+
 from itemadapter import ItemAdapter
 
 
 class SpiderwhoPipeline:
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+
+    def create_connection(self):
+        conn = sqlite3.connect('myquotes.db')
+        curs = conn.cursor()
+
+    def create_table(self):
+        self.curs.execute("DROP TABLE IF EXISTS quotes_tb")
+
+        self.sql=''' create table quotes_tb(
+                         title text,
+                         author text,
+                         tag text) '''
+        self.curs.execute(self.sql)
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+
+    def store_db(self, item):
+          self.curs.execute(''' insert into quotes_tb values (%s, %s, %s)''',(
+              item['question'][0],
+              item['response'][0]
+          ))
+          self.conn.commit()
